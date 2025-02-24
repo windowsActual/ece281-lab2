5295ad3f600c34550755b383d45232f84addeedc
windowsActual <177792113+windowsActual@users.noreply.github.com>
2025-02-24 08:34:39 -0700

    Instructions told me to commit here

---
+++ src/sevenseg_decoder.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/24/2025 08:32:41 AM
+-- Design Name: 
+-- Module Name: sevenseg_decoder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sevenseg_decoder is
+    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+           o_sen_n : out STD_LOGIC_VECTOR (6 downto 0));
+end sevenseg_decoder;
+
+architecture Behavioral of sevenseg_decoder is
+
+begin
+
+
+end Behavioral;
